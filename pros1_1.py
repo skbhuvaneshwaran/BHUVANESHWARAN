7ca53a10fe3f33e06eaa8f4af7cccf619b3a0021
skbhuvaneshwaran <51233291+skbhuvaneshwaran@users.noreply.github.com>
2019-08-11 14:53:51 +0530

    Update pros1_1.py

--- pros1_1.py
+++ pros1_1.py
@@ -1,12 +1,12 @@
-a=int(input())
-li=[]
-for i in range(0,a):
-  b=list(map(str,input()))
-  li.append(b)
-c=zip(*li)
-d=list(c)
-for i in d:
-  if(len(i)==i.count(i[0])):
-    print(i[0], end='')
+l=[]
+i=int(input("no of strings"))
+for j in range(0,i):
+  k=list(input("enter string"))
+  l.append(k)
+ m=zip(*l)
+n=list(m)
+for j in n:
+  if(len(j)==j.count(j[0])):
+    print(j[0],end=" ")
   else:
     break
